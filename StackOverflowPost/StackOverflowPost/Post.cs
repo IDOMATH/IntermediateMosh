101a78a22fe40e552f1fc432408cc05520ccf601
IDOMATH <btdothemath@gmail.com>
2020-11-09 16:10:09 -0600

    Finished StackOverflow Post assignment
    assignment is done and works as intended

---
+++ StackOverflowPost/StackOverflowPost/Post.cs
@@ -0,0 +1,28 @@
+﻿namespace StackOverflowPost
+{
+    public class Post
+    {
+        private readonly string Title;
+        public int Score { get; private set; }
+        public string Description { get; set; }
+
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+        }
+
+        public void Upvote()
+        {
+            Score++;
+        }
+        public void Downvote()
+        {
+            Score--;
+        }
+        public void DisplayPost()
+        {
+            System.Console.WriteLine("Title: {0}\n{1}", Title, Description);
+        }
+    }
+}
---
+++ StackOverflowPost/StackOverflowPost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post myPost = new Post("Help, I need something", "there is a problem, here it is. Help.");
+
+            for (int i = 0; i < 10; i++) myPost.Upvote();
+            myPost.Downvote();
+            Console.WriteLine("Score: {0}", myPost.Score);
+        }
+    }
+}
